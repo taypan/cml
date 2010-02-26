64e25ad6c4224cef29b5b72f01fe08818d04189a
Taypan <taypan@email.cz>
2010-02-26 21:41:58 +0100

    pridan rozcestnik

--- modules/Tester.php
+++ modules/Tester.php
@@ -9,9 +9,41 @@ var $acl = array(	"test" => "guest",
 function get_default($n){
 
 //$sum = $this->ack($m,$n);
-$_SESSION['items'] = array(72,71);
+global $database;
+$q = "DELETE FROM texty WHERE jmeno = 'rozcestnik'";
+$p = "insert into texty values ('','rozcestnik','<div id=\"obsahSekce\"><div class=\"sekceCont\"> <a href=\"#\">
+       <h2>Čajové stolky a sety</h2>
+       </a>
+       <div class=\"linkCont\"> <a href=\"#\">jidelni</a> <a href=\"#\">konferencni</a> <a href=\"#\">odkladaci</a> </div>
+     </div>
+     <div class=\"sekceCont\"> <a href=\"#\">
+       <h2>Ložnice</h2>
+       </a>
+       <div class=\"linkCont\"> <a href=\"#\">jidelni</a> <a href=\"#\">konferencni</a> <a href=\"#\">odkladaci</a> </div>
+     </div>
+     <div class=\"sekceCont\"> <a href=\"#\">
+       <h2>STOLY A STOLKY</h2>
+       </a>
+       <div class=\"linkCont\"> <a href=\"#\">jidelni</a> <a href=\"#\">konferencni</a> <a href=\"#\">odkladaci</a> </div>
+     </div>
+     <div class=\"sekceCont\"> <a href=\"index.php?page=Zbozi&limit=0&cat=stoly\">
+       <h2>PRACOVNí STOLY A SKříňKY</h2>
+       </a>
+       <div class=\"linkCont\"> <a href=\"index.php?page=Zbozi&limit=0&cat=stoly&subcat=jidelni\">jidelni</a> <a href=\"#\">konferencni</a> <a href=\"#\">odkladaci</a> </div>
+     </div>
+     <div class=\"sekceCont\"> <a href=\"#\">
+       <h2>Dětský nábytek</h2>
+       </a>
+       <div class=\"linkCont\"> <a href=\"#\">jidelni</a> <a href=\"#\">konferencni</a> <a href=\"#\">odkladaci</a> </div>
+     </div>
+     <div class=\"sekceCont\"> <a href=\"#\">
+       <h2>Zakázková výroba</h2>
+       </a>
+       <div class=\"linkCont\"> <a href=\"#\">jidelni</a> <a href=\"#\">konferencni</a> <a href=\"#\">odkladaci</a> </div>
+     </div>','Nábytek')";
 
-
+$database->query($q);
+$database->query($p);
 return "OK";
 }
 
