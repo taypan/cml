d0d91e976a006b2b65cddb3a54003a48ce592fae
Taypan <taypan@email.cz>
2010-03-25 07:24:35 +0100

    zalozeni samorostu

--- modules/Kategorie.php
+++ modules/Kategorie.php
@@ -3,7 +3,6 @@ class Kategorie extends Modul{
 
 var $acl = array(	"test" => "guest",
 					"get_default" => "guest",
-					"test2" => "administrator",
 					"nevim" => "administrator");
 
 function get_default($n){
